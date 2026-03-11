c7d629dc278df17798d49d216835dbe02577760f
Matteo Codogno <matteo.codogno@welld.ch>
2026-03-11 17:11:57 +0100

    feat(backend): enable scheduled task execution
    - enable the application to detect and run methods annotated with
      @Scheduled
    - provide a foundation for implementing future background jobs and
      automated tasks

--- backend/src/main/kotlin/io/stageboard/spellbook/StageboardApplication.kt
+++ backend/src/main/kotlin/io/stageboard/spellbook/StageboardApplication.kt
@@ -2,7 +2,9 @@ package io.stageboard.spellbook
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class StageboardApplication
 
